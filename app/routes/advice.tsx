f4bc583c8fbd34ae677aae28ccb39ca0d8718184
Anum <hello@anum.io>
2024-04-09 23:58:57 +0200

    Use Aksel komponents to visualize the elements on advice page

--- app/routes/advice.tsx
+++ app/routes/advice.tsx
@@ -1,4 +1,20 @@
+import {
+  BodyShort,
+  Box,
+  HStack,
+  Heading,
+  Page,
+  VStack,
+} from "@navikt/ds-react";
 import type { MetaFunction } from "@remix-run/node";
+import {
+  Chat2Icon,
+  HandshakeIcon,
+  LightBulbIcon,
+  PencilIcon,
+  TrendUpIcon,
+} from "@navikt/aksel-icons";
+
 import "@styles/advice.scss";
 
 export const meta: MetaFunction = () => {
@@ -14,83 +30,141 @@ export const meta: MetaFunction = () => {
 
 export default function Advice() {
   return (
-    <div className="advice">
-      <h1>Advice for Job Seekers</h1>
-      <p>
-        Welcome to our Advice Section. Here you'll find tips and strategies to
-        boost your job search, ace your interviews, and advance your career.
-      </p>
+    <Page.Block gutters width="xl">
+      <Box padding={{ xs: "4", sm: "8", md: "12", lg: "16" }}>
+        <VStack as="main" className="advice" gap="8">
+          <VStack gap="4">
+            <HStack align="center" gap="1">
+              <Heading size="large" level="1">
+                Advice for Job Seekers
+              </Heading>
+            </HStack>
+            <BodyShort>
+              Welcome to our Advice Section. Here you'll find tips and
+              strategies to boost your job search, ace your interviews, and
+              advance your career.
+            </BodyShort>
+          </VStack>
 
-      <article>
-        <h2>Top Resume Tips to Stand Out</h2>
-        <p>
-          Your resume is your first impression on potential employers. Learn how
-          to highlight your strengths, tailor your resume for different roles,
-          and avoid common mistakes.
-        </p>
-        <p>
-          Ensure your resume is clear, concise, and up to date. Use action verbs
-          to describe your achievements and tailor your resume for each
-          application to highlight the most relevant experience.
-        </p>
-      </article>
+          <VStack gap="4" as="article">
+            <HStack align="center" gap="1">
+              <LightBulbIcon
+                title="Top Resume Tips to Stand Out"
+                fontSize={"1.5rem"}
+              />
+              <Heading size="medium" level="2">
+                Top Resume Tips to Stand Out
+              </Heading>
+            </HStack>
+            <VStack>
+              <BodyShort>
+                Your resume is your first impression on potential employers.
+                Learn how to highlight your strengths, tailor your resume for
+                different roles, and avoid common mistakes.
+              </BodyShort>
+              <BodyShort>
+                Ensure your resume is clear, concise, and up to date. Use action
+                verbs to describe your achievements and tailor your resume for
+                each application to highlight the most relevant experience.
+              </BodyShort>
+            </VStack>
+          </VStack>
 
-      <article>
-        <h2>How to Prepare for an Interview</h2>
-        <p>
-          Preparation is key to interview success. Discover techniques to
-          confidently answer common questions, make a lasting impression, and
-          follow up effectively post-interview.
-        </p>
-        <p>
-          Research the company and understand its culture, products, and the
-          industry it operates in. Practice your responses to common interview
-          questions with a friend or mentor.
-        </p>
-      </article>
+          <VStack as="article" gap="4">
+            <HStack align="center" gap="1">
+              <PencilIcon
+                title="How to Prepare for an Interview"
+                fontSize="1.5rem"
+              />
 
-      <article>
-        <h2>Effective Networking Strategies</h2>
-        <p>
-          Building a professional network can open doors to opportunities.
-          Uncover ways to network authentically, leverage social media, and
-          create meaningful connections.
-        </p>
-        <p>
-          Attend industry meetups, conferences, and seminars. Be proactive in
-          your outreach on LinkedIn and other professional platforms. Offer
-          value in your interactions, rather than seeking benefits only.
-        </p>
-      </article>
+              <Heading size="medium" level="2">
+                How to Prepare for an Interview
+              </Heading>
+            </HStack>
+            <BodyShort>
+              Preparation is key to interview success. Discover techniques to
+              confidently answer common questions, make a lasting impression,
+              and follow up effectively post-interview.
+            </BodyShort>
+            <BodyShort>
+              Research the company and understand its culture, products, and the
+              industry it operates in. Practice your responses to common
+              interview questions with a friend or mentor.
+            </BodyShort>
+          </VStack>
 
-      <article>
-        <h2>Negotiating Salaries: Do's and Don'ts</h2>
-        <p>
-          Negotiating your salary doesn't have to be daunting. Learn tactics to
-          approach salary discussions with confidence and secure a compensation
-          package that reflects your value.
-        </p>
-        <p>
-          Do your research on industry standards, and approach the negotiation
-          with a clear understanding of what you want and what you can
-          realistically get. Be prepared to articulate your value and
-          contributions.
-        </p>
-      </article>
+          <VStack as="article" gap="4">
+            <HStack align="center" gap="1">
+              <HandshakeIcon
+                title="Effective Networking Strategies"
+                fontSize="1.5rem"
+              />
 
-      <article>
-        <h2>Strategies for Career Advancement</h2>
-        <p>
-          Advancing in your career requires more than just hard work. Explore
-          strategies for professional growth, seeking out mentorship, and
-          embracing lifelong learning.
-        </p>
-        <p>
-          Seek feedback regularly, take on new challenges, and be open to
-          change. Consider additional certifications or courses that can enhance
-          your skill set and make you a more valuable asset to your team.
-        </p>
-      </article>
-    </div>
+              <Heading size="medium" level="2">
+                Effective Networking Strategies
+              </Heading>
+            </HStack>
+            <BodyShort>
+              Building a professional network can open doors to opportunities.
+              Uncover ways to network authentically, leverage social media, and
+              create meaningful connections.
+            </BodyShort>
+            <BodyShort>
+              Attend industry meetups, conferences, and seminars. Be proactive
+              in your outreach on LinkedIn and other professional platforms.
+              Offer value in your interactions, rather than seeking benefits
+              only.
+            </BodyShort>
+          </VStack>
+
+          <VStack as="article" gap="4">
+            <HStack align="center" gap="1">
+              <Chat2Icon
+                title="Negotiating Salaries: Do's and Don'ts"
+                fontSize="1.5rem"
+              />
+              <Heading size="medium" level="2">
+                Negotiating Salaries: Do's and Don'ts
+              </Heading>
+            </HStack>
+            <BodyShort>
+              Negotiating your salary doesn't have to be daunting. Learn tactics
+              to approach salary discussions with confidence and secure a
+              compensation package that reflects your value.
+            </BodyShort>
+            <BodyShort>
+              Do your research on industry standards, and approach the
+              negotiation with a clear understanding of what you want and what
+              you can realistically get. Be prepared to articulate your value
+              and contributions.
+            </BodyShort>
+          </VStack>
+
+          <VStack as="article" gap="4">
+            <HStack align="center" gap="1">
+              <TrendUpIcon
+                title="Strategies for Career Advancement"
+                fontSize="1.5rem"
+              />
+
+              <Heading size="medium" level="2">
+                Strategies for Career Advancement
+              </Heading>
+            </HStack>
+            <BodyShort>
+              Advancing in your career requires more than just hard work.
+              Explore strategies for professional growth, seeking out
+              mentorship, and embracing lifelong learning.
+            </BodyShort>
+            <BodyShort>
+              Seek feedback regularly, take on new challenges, and be open to
+              change. Consider additional certifications or courses that can
+              enhance your skill set and make you a more valuable asset to your
+              team.
+            </BodyShort>
+          </VStack>
+        </VStack>
+      </Box>
+    </Page.Block>
   );
 }
