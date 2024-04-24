057ba890718566515d886f26b59a3366521ed497
Anum <hello@anum.io>
2024-04-24 11:43:50 +0200

    Set default to dark mode

--- app/contexts/GlobalContext.tsx
+++ app/contexts/GlobalContext.tsx
@@ -2,7 +2,7 @@ import { ReactNode, createContext, useContext, useMemo, useState } from "react";
 
 const defaultValue = {
   isLiveMode: false,
-  isDarkMode: false,
+  isDarkMode: true,
   toggleLiveMode: () => {},
   toggleDarkMode: () => {},
 };
